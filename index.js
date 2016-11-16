f93f7a4fb80797e6da3f23d44f5984d70a2d9964
fuerbringer <severin@protonmail.ch>
2016-11-16 09:49:43 +0100

    Added todoadd functionality.

--- index.js
+++ index.js
@@ -56,17 +56,17 @@ jsonfile.readFile(file, function(err, obj) {
 	 * TODO: remove this
 	 */
 	bot.onText(/\/ripDB/, function (msg, match) {
-		var chatId = msg.chat.id;
+		var tg_id = msg.chat.id;
 		var resp = "";
 
-		connection.execute('SELECT * FROM test', function (err, results, fields) {
+		connection.execute('SELECT * FROM todo', function (err, results, fields) {
 
 			for (var i = 0; i < results.length; i++) {
 				console.log(results[i].todotxt);
 				resp += results[i].todotxt;
 			}
 
-			bot.sendMessage(chatId, resp);
+			bot.sendMessage(tg_id, resp);
 		});
 
 	});
@@ -74,19 +74,39 @@ jsonfile.readFile(file, function(err, obj) {
 	/**
 	 * 
 	 */
-	bot.onText(/\/insert (.+)/, function (msg, match) {
-		var chatId = msg.chat.id;
+	bot.onText(/\/todoadd (.+)/, function (msg, match) {
+		var tg_id = msg.chat.id;
+		var chat_id = 0;
 
-		var sql = "INSERT INTO test (todotxt) VALUES (?)";
+		// Save chat ID if it doesn't already exist
+		var sql = "INSERT IGNORE INTO chat (tg_id) VALUES (?)";
+		connection.execute(sql, [tg_id], function (err, results, fields) {
+			bot.sendMessage(tg_id, "Added new chat " + tg_id + " " + err);
+		});
 
+		// Fetch chat.id
+		/*
+		var sql = "SELECT id FROM chat WHERE tg_id=?";
+		connection.execute(sql, [tg_id], function (err, results, fields) {
+			chat_id = results[0].id;
+			bot.sendMessage(tg_id, "Found chat " + chat_id + " " + err);
+		});
+		*/
+	
+
+		// Insert todo
+		var sql = "INSERT INTO todo (chat_id, value) VALUES ((SELECT id FROM chat WHERE tg_id='"+tg_id+"'),?)";
 		connection.execute(sql, [match[1]], function (err, results, fields) {
 
-			var output = sql + "\n" + results + "\n" + fields;
-
-			bot.sendMessage(chatId, output);
+			var output = tg_id + " " + chat_id + " err: " + err;
+			bot.sendMessage(tg_id, output);
 		});
 
 	});
+
+	/**
+	 * TODO: add todolist
+	 */
 
 	/**
 	 * TODO: remove this
