058cf1125e85022400fd95efe4f511c6a712ffb7
Gianluca Scarciolla <gianluca.s@mchd.ch>
2016-11-29 09:29:46 +0100

    Added Prototype Code for PDF to Text
    NOT FINISHED YET!

--- index.js
+++ index.js
@@ -53,6 +53,36 @@ jsonfile.readFile('auth.json', function(err, obj) {
 	});
 
 	/**
+	 * PDF to TEXT //Luu
+	 */
+	bot.onText(/\/pdftotext (.+)/, function (msg, match) {
+		var chatId = msg.chat.id;
+		var url = match[1];
+		
+		/**
+		var pdfUtil = require('pdf-to-text');
+		var pdf_path = "absolute_path/to/pdf_file.pdf";
+ 
+		//option to extract text from page 0 to 10 
+		var option = {from: 0, to: 10};
+ 
+		pdfUtil.pdfToText(upload.path, option, function(err, data) {
+		  if (err) throw(err);
+		  bot.sendMessage(chatId, data); //print text     
+		});
+ 
+		//Omit option to extract all text from the pdf file 
+		pdfUtil.pdfToText(upload.path, function(err, data) {
+		  if (err) throw(err);
+		  console.log(data); //print all text     
+		});
+		*/
+		
+		resp = "Coming soon! :D";
+		bot.sendMessage(chatId, resp);
+	});
+
+	/**
 	 * `clears` screen with N * newlines
 	 */
 	bot.onText(/\/clr (.+)/, function (msg, match) {
